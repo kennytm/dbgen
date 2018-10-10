a9554cff7a9f917d03efde40be7527cc008ec4e7
kennytm <kennytm@gmail.com>
2018-10-11 00:13:38 +0800

    Fixed syntax error.

--- res/sysbench_oltp_uniform.sql
+++ res/sysbench_oltp_uniform.sql
@@ -2,11 +2,11 @@ CREATE TABLE sbtest1 (
     id  INTEGER NOT NULL AUTO_INCREMENT,
         {{ rownum }}
     k   INTEGER DEFAULT '0' NOT NULL,
-        {{ rand.range_inclusive(0, 0xffffffff) }}
+        {{ rand.range_inclusive(-0x80000000, 0x7fffffff) }}
     c   CHAR(120) DEFAULT '' NOT NULL,
         {{ rand.regex('([0-9]{11}-){9}[0-9]{11}') }}
     pad CHAR(60) DEFAULT '' NOT NULL,
         {{ rand.regex('([0-9]{11}-){4}[0-9]{11}') }}
     PRIMARY KEY(id),
-    INDEX KEY(k)
+    INDEX(k)
 );
