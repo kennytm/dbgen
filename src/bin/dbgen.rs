5b7ae3015100c26d1c1716ee657f2e4a2143c0ad
kennytm <kennytm@gmail.com>
2018-10-11 18:38:07 +0800

    Record number of bytes written to provide the byte speed progress

--- src/bin/dbgen.rs
+++ src/bin/dbgen.rs
@@ -18,7 +18,7 @@ use dbgen::{
 
 use data_encoding::{DecodeError, DecodeKind, HEXLOWER_PERMISSIVE};
 use failure::{Error, Fail, ResultExt};
-use pbr::ProgressBar;
+use pbr::{MultiBar, Units};
 use rand::{EntropyRng, Rng, SeedableRng, StdRng};
 use rayon::{
     iter::{IntoParallelIterator, ParallelIterator},
@@ -26,7 +26,7 @@ use rayon::{
 };
 use std::{
     fs::{create_dir_all, read_to_string, File},
-    io::{BufWriter, Write},
+    io::{self, BufWriter, Write},
     path::{Path, PathBuf},
     process::exit,
     sync::atomic::{AtomicBool, AtomicUsize, Ordering},
@@ -142,6 +142,7 @@ impl<T, E: Fail> PathResultExt for Result<T, E> {
 
 static WRITE_FINISHED: AtomicBool = AtomicBool::new(false);
 static WRITE_PROGRESS: AtomicUsize = AtomicUsize::new(0);
+static WRITTEN_SIZE: AtomicUsize = AtomicUsize::new(0);
 
 fn run() -> Result<(), Error> {
     let args = Args::from_args();
@@ -186,12 +187,51 @@ fn run() -> Result<(), Error> {
 
     let progress_bar_thread = spawn(move || {
         let total_rows = u64::from(files_count) * rows_per_file;
-        let mut pb = ProgressBar::new(total_rows);
+        let mut mb = MultiBar::new();
+
+        let mut pb = mb.create_bar(total_rows);
+
+        let mut speed_bar = mb.create_bar(0);
+        speed_bar.set_units(Units::Bytes);
+        speed_bar.show_percent = false;
+        speed_bar.show_time_left = false;
+        speed_bar.show_tick = true;
+        speed_bar.show_bar = false;
+        #[cfg_attr(feature = "cargo-clippy", allow(clippy::non_ascii_literal))]
+        {
+            speed_bar.tick_format("🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚🕛");
+        }
+
+        pb.message("Progress ");
+        speed_bar.message("Size     ");
+
+        let mb_thread = spawn(move || mb.listen());
+
         while !WRITE_FINISHED.load(Ordering::Relaxed) {
             sleep(Duration::from_millis(500));
-            pb.set(WRITE_PROGRESS.load(Ordering::Relaxed) as u64);
+            let rows_count = WRITE_PROGRESS.load(Ordering::Relaxed);
+            pb.set(rows_count as u64);
+
+            let written_size = WRITTEN_SIZE.load(Ordering::Relaxed);
+            #[cfg_attr(
+                feature = "cargo-clippy",
+                allow(
+                    clippy::cast_precision_loss,
+                    clippy::cast_possible_truncation,
+                    clippy::cast_sign_loss
+                )
+            )]
+            {
+                let estimated_total = (written_size as f64) * (total_rows as f64) / (rows_count as f64);
+                speed_bar.total = estimated_total as u64;
+                speed_bar.set(written_size as u64);
+            }
         }
+
         pb.finish_println("Done!");
+        speed_bar.finish();
+
+        mb_thread.join().unwrap();
     });
 
     let iv = (0..files_count)
@@ -219,6 +259,32 @@ struct Env {
     rows_count: u32,
 }
 
+struct WriteCountWrapper<W: Write> {
+    inner: W,
+    count: usize,
+}
+impl<W: Write> WriteCountWrapper<W> {
+    fn new(inner: W) -> Self {
+        Self { inner, count: 0 }
+    }
+
+    fn commit_bytes_written(&mut self) {
+        WRITTEN_SIZE.fetch_add(self.count, Ordering::Relaxed);
+        self.count = 0;
+    }
+}
+
+impl<W: Write> Write for WriteCountWrapper<W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let bytes_written = self.inner.write(buf)?;
+        self.count += bytes_written;
+        Ok(bytes_written)
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        self.inner.flush()
+    }
+}
+
 impl Env {
     fn write_schema(&self, content: &str) -> Result<(), Error> {
         let path = self.out_dir.join(format!("{}-schema.sql", self.unique_name));
@@ -231,9 +297,10 @@ impl Env {
             "{0}.{1:02$}.sql",
             self.unique_name, file_index, self.file_num_digits
         ));
-        let mut file = BufWriter::new(File::create(&path).with_path(&path)?);
+        let mut file = WriteCountWrapper::new(BufWriter::new(File::create(&path).with_path(&path)?));
         for _ in 0..self.inserts_count {
             writeln!(file, "INSERT INTO {} VALUES", self.qualified_name).with_path(&path)?;
+
             for row_index in 0..self.rows_count {
                 self.row_gen.write_sql(state, &mut file).with_path(&path)?;
                 file.write_all(if row_index == self.rows_count - 1 {
@@ -243,6 +310,8 @@ impl Env {
                 })
                 .with_path(&path)?;
             }
+
+            file.commit_bytes_written();
             WRITE_PROGRESS.fetch_add(self.rows_count as usize, Ordering::Relaxed);
         }
         Ok(())
