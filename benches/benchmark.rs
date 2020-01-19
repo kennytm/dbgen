aa9105973feaa88e1310b2d6eccb1da963aebed7
kennytm <kennytm@gmail.com>
2020-01-20 00:53:18 +0800

    Added a simple benchmark of writing rows

---
+++ benches/benchmark.rs
@@ -0,0 +1,48 @@
+use anyhow::Error;
+use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
+use dbgen::{
+    eval::{CompileContext, State},
+    format::{Format, SqlFormat},
+    parser::Template,
+};
+use rand::SeedableRng;
+use rand_hc::Hc128Rng;
+use std::{
+    fs::read_to_string,
+    io::{sink, Write},
+};
+
+fn run_benchmark(b: &mut Bencher<'_>, path: &str) -> Result<(), Error> {
+    let template = Template::parse(&read_to_string(path)?)?;
+    let ctx = CompileContext::default();
+    let row = ctx.compile_row(template.exprs)?;
+    let mut state = State::new(
+        1,
+        Box::new(Hc128Rng::from_seed([0x41; 32])),
+        template.variables_count,
+        ctx,
+    );
+    let format = SqlFormat {
+        escape_backslash: false,
+    };
+    let mut sink: Box<dyn Write> = Box::new(sink());
+
+    b.iter(move || -> Result<(), Error> {
+        let values = black_box(&row).eval(black_box(&mut state))?;
+        for value in values {
+            format.write_value(black_box(&mut *sink), &value)?;
+        }
+        Ok(())
+    });
+
+    Ok(())
+}
+
+fn bench_templates(c: &mut Criterion) {
+    c.bench_function("sysbench_oltp_uniform", |b| {
+        run_benchmark(b, "res/sysbench/oltp_uniform_mysql.sql").unwrap();
+    });
+}
+
+criterion_group!(benches, bench_templates);
+criterion_main!(benches);
