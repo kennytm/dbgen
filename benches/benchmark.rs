d6f292f537f55cade748017d457a436a2d202724
kennytm <kennytm@gmail.com>
2021-02-13 17:34:17 +0800

    Fixed benchmark code.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,9 +1,9 @@
-use anyhow::Error;
 use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
 use dbgen::{
     eval::{CompileContext, State},
     format::{Format, SqlFormat},
     parser::Template,
+    span::Registry,
 };
 use rand::SeedableRng;
 use rand_hc::Hc128Rng;
@@ -12,30 +12,28 @@ use std::{
     io::{sink, Write},
 };
 
-fn run_benchmark(b: &mut Bencher<'_>, path: &str) -> Result<(), Error> {
-    let mut template = Template::parse(&read_to_string(path)?, &[], None)?;
+fn run_benchmark(b: &mut Bencher<'_>, path: &str) {
+    let mut registry = Registry::default();
+    let mut template = Template::parse(&read_to_string(path).unwrap(), &[], None, &mut registry).unwrap();
     let ctx = CompileContext::new(template.variables_count);
-    let row = ctx.compile_row(template.tables.swap_remove(0).exprs)?;
+    let row = ctx.compile_row(template.tables.swap_remove(0).exprs).unwrap();
     let mut state = State::new(1, Box::new(Hc128Rng::from_seed([0x41; 32])), ctx);
     let format = SqlFormat {
         escape_backslash: false,
     };
     let mut sink: Box<dyn Write> = Box::new(sink());
 
-    b.iter(move || -> Result<(), Error> {
-        let values = black_box(&row).eval(black_box(&mut state))?;
+    b.iter(move || {
+        let values = black_box(&row).eval(black_box(&mut state)).unwrap();
         for value in values {
-            format.write_value(black_box(&mut *sink), &value)?;
+            format.write_value(black_box(&mut *sink), &value).unwrap();
         }
-        Ok(())
     });
-
-    Ok(())
 }
 
 fn bench_templates(c: &mut Criterion) {
     c.bench_function("sysbench_oltp_uniform", |b| {
-        run_benchmark(b, "res/sysbench/oltp_uniform_mysql.sql").unwrap();
+        run_benchmark(b, "res/sysbench/oltp_uniform_mysql.sql");
     });
 }
 
